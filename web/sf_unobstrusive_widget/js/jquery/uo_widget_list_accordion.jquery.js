bfb5c70d7b0051ee73778f7dbad32de4fbb402e9
francois.b <francois.b@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-01-30 13:02:27 -0600

    [sfUnobstrusiveWidgetPlugin] javascript refactoring
    git-svn-id: http://svn.symfony-project.com/plugins/sfUnobstrusiveWidgetPlugin/trunk@15118 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- web/sf_unobstrusive_widget/js/jquery/uo_widget_list_accordion.jquery.js
+++ web/sf_unobstrusive_widget/js/jquery/uo_widget_list_accordion.jquery.js
@@ -1,43 +1,74 @@
 /**
- * Initialize an unobstrusive accordion widget using jQuery.
- * Match all DIV with "uo_widget_list_accordion" class.
+ * Unobstrusive accordion widget using jQuery.
  *
  * @author     François Béliveau <francois.beliveau@my-labz.com>
  */
-var uo_widget_list_accordion_config      = {};
-$(document).ready(function()
-{
-  $('ul.uo_widget_list_accordion').each(function(i)
+var uo_widget_list_accordion_config = {};
+(function($) {
+
+  $.fn.uoWidgetListAccordion = function(customConfiguration)
   {
-    if ($(this).hasClass('uo_widget_list_accordion_ON'))
+    // default configuration
+    var configuration = {};
+
+    // merge default and custom configuration
+    $.extend(true, configuration, customConfiguration);
+
+    return this.each(function(index)
     {
-      return $(this);
-    }
-  
-    var id        = $(this).attr('id');
-    var config    = {};
-    if (undefined != uo_widget_list_accordion_config[id])
-    {
-      config = uo_widget_list_accordion_config[id];
-    }
-    
-    //create A element
-    $('.uo_widget_accordion_title', this).each(function()
-    {
-      var firstchild = this.firstChild;
-      if ('a' != firstchild.nodeName.toLowerCase())
+      var $widget = $(this);
+
+      /**
+       * Initialize widget
+       */
+      function init()
       {
-        $(firstchild).before('<a href="#"></a>');
-        $(firstchild).appendTo(this.firstChild);
+        // prevent initialize twice
+        if ($widget.hasClass('uo_widget_list_accordion_ON'))
+        {
+          return $widget;
+        }
+
+        //create A element
+        $('.uo_widget_accordion_title', $widget).each(function()
+        {
+          var firstchild = this.firstChild;
+          if ('a' != firstchild.nodeName.toLowerCase())
+          {
+            $(firstchild).before('<a href="#"></a>');
+            $(firstchild).appendTo(this.firstChild);
+          }
+        });
+
+        $widget.removeClass('uo_widget_list_accordion');
+        $widget.addClass('uo_widget_list_accordion_ON');
+        $widget.accordion(getConfiguration());
       }
-    });
-    
-    $(this).addClass('uo_widget_list_accordion_ON');
-    $(this).removeClass('uo_widget_list_accordion');
-    
-    config.active = '.uo_widget_accordion_active',
-    config.header = '.uo_widget_accordion_title',
       
-    $(this).accordion(config);
-	});
+      /**
+       * Return widget's specific configuration
+       */
+      function getConfiguration()
+      {
+        var config    = uo_widget_list_accordion_config[$widget.attr('id')] || {};
+        config.active = '.uo_widget_accordion_active';
+        config.header = '.uo_widget_accordion_title';
+        
+        return config;
+      }
+
+      init();
+    });
+
+  };
+
+})(jQuery);
+
+/**
+ * Initialize widget.
+ * Match all UL or OL with "uo_widget_list_accordion" class.
+ */
+jQuery(document).ready(function()
+{
+  $('ul.uo_widget_list_accordion, ol.uo_widget_list_accordion').uoWidgetListAccordion({})
 });
