534d63f24918aa4d9dc33a9cfc5fd5779a363fdf
francois.b <francois.b@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-03-26 13:35:42 -0500

    fix sfUoWidgetFormTextarea test
    git-svn-id: http://svn.symfony-project.com/plugins/sfUnobstrusiveWidgetPlugin/trunk@16636 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/widget_form/sfUoWidgetFormTextareaTest.php
+++ test/unit/widget_form/sfUoWidgetFormTextareaTest.php
@@ -4,7 +4,7 @@ $pluginPath = dirname(__FILE__).'/../../..';
 include($pluginPath.'/test/bootstrap.php');
 
 require_once($pluginPath.'/lib/widget/base/sfUoWidget.class.php');
-require_once($pluginPath.'/lib/widget_form/sfUoWidgetFormTextArea.class.php');
+require_once($pluginPath.'/lib/widget_form/sfUoWidgetFormTextarea.class.php');
 
 
 $t = new lime_test(0, new lime_output_color());
