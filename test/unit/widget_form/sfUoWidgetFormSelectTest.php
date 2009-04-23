564c523db28f0e256dc2210bdc95182bc26bdc94
francois.b <francois.b@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-04-23 02:18:27 -0500

    [sfUnobstrusiveWidgetPlugin] fix widget form delect tests
    git-svn-id: http://svn.symfony-project.com/plugins/sfUnobstrusiveWidgetPlugin/trunk@17556 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- test/unit/widget_form/sfUoWidgetFormSelectTest.php
+++ test/unit/widget_form/sfUoWidgetFormSelectTest.php
@@ -6,7 +6,7 @@ include($pluginPath.'/test/bootstrap.php');
 require_once($pluginPath.'/lib/widget/base/sfUoWidget.class.php');
 require_once($pluginPath.'/lib/widget_form/sfUoWidgetFormSelect.class.php');
 
-$t = new lime_test(17, new lime_output_color());
+$t = new lime_test(15, new lime_output_color());
 
 $dom = new DomDocument('1.0', 'utf-8');
 $dom->validateOnParse = true;
