5fe65572138a211420a0ad6edac613a99f0b7dd2
francois.b <francois.b@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-05-26 18:51:10 -0500

    [sfUnobstrusiveWidgetPlugin] refactoring
    git-svn-id: http://svn.symfony-project.com/plugins/sfUnobstrusiveWidgetPlugin/branches/beta_1.1@18676 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/widget/sfUoWidgetMenu.class.php
+++ lib/widget/sfUoWidgetMenu.class.php
@@ -18,9 +18,6 @@
  */
 class sfUoWidgetMenu extends sfUoWidgetList
 {
-  protected
-    $controller = null;
-  
   /**
    * Configures the current widget.
    *
@@ -35,20 +32,7 @@ class sfUoWidgetMenu extends sfUoWidgetList
   {
     parent::configure($options, $attributes);
     
-    $this->addOption('controller', null);
     $this->addOption('active', null);
-  }
-  
-  /**
-   * @return string An HTML tag string
-   *
-   * @see render()
-   */
-  protected function doRender()
-  {
-    $this->controller = $this->getOption('controller') ? $this->getOption('controller') : sfContext::getInstance()->getController();
-
-    return parent::doRender();
   }
 
   /**
@@ -109,7 +93,7 @@ class sfUoWidgetMenu extends sfUoWidgetList
       }
       else
       {
-        $result = $this->controller->genUrl($values['route'], array_key_exists('absolute', $values) && $values['absolute']);
+        $result = $this->getController()->genUrl($values['route'], array_key_exists('absolute', $values) && $values['absolute']);
       }
     }
     
