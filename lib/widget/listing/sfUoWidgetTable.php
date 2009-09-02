7a6c1506ee00bcdad490614af555d46e3febcfb8
francois.b <francois.b@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-09-02 04:38:05 -0500

    [sfUnobstrusiveWidgetPlugin] refactoring on sfUoWidgetTable widget
    git-svn-id: http://svn.symfony-project.com/plugins/sfUnobstrusiveWidgetPlugin/trunk@21634 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/widget/listing/sfUoWidgetTable.php
+++ lib/widget/listing/sfUoWidgetTable.php
@@ -23,12 +23,7 @@ class sfUoWidgetTable extends sfUoWidget
     return count($this->getOption('data'));
   }
   
-  /**
-   * Configures the current widget.
-   *
-   * Available options:
-   *
-   *  * choice_enable:                 Enable or not the choice in each row
+  *  * choice_enable:                 Enable or not the choice in each row
    *  * choice_name:                   Choice input name
    *  * choice_type:                   Choice type ("checkbox" or "radio")
    *  * choice_position:               Checkbox position ("first" or "last")
@@ -45,6 +40,47 @@ class sfUoWidgetTable extends sfUoWidget
    *  * header_template_extra_vars:    Header template extra vars
    *  * footer_template_name:          Footer template name
    *  * footer_template_extra_vars:    Footer template extra vars
+  
+  
+  
+  /**
+   * Configures the current widget.
+   *
+   * Available options:
+   *
+   *  * data:                                    Data to inject in the table body
+   *  * data_header:                             Data to inject in the table header
+   *  * data_footer:                             Data to inject in the table footer
+   *  * 
+   *  * no_data_message:                         Message to display when data is empty
+   *  * no_data_classname:                       CSS classname added to no data message container
+   *  *
+   *  * choice_enable:                           Enable or not the choice in each row, false by default
+   *  * choice_name:                             Choice input name, "items" by default
+   *  * choice_type:                             Choice type ("checkbox" or "radio"), "checkbox" by default
+   *  * choice_position:                         Checkbox position ("first" or "last"), "last" by default
+   *  * choice_header:                           Content of the choice header, "&nbsp;" by default
+   *  * choice_footer:                           Content of the choice footer, "&nbsp;" by default
+   *  * choice_classname:                        CSS classname added to choice column, "choice" by default
+   *  *
+   *  * row_number_enable:                       Enable or not a number in each row, false by default
+   *  * row_number_start:                        Row number start value
+   *  * row_number_header:                       Content of the row number header, "#" by default
+   *  * row_number_footer:                       Content of the row number footer, "&nbsp;" by default
+   *  * row_number_classname:                    CSS classname added to row number column, "number" by default
+   *  * 
+   *  * row_classname_enable:                    Enable or not the class in each row, true by default
+   *  * row_classname_odd:                       CSS classname added to each odd row, "odd" by default
+   *  * row_classname_even:                      CSS classname added to each even row, "even" by default
+   *  *
+   *  * row_template_name:                       Row template path to use to render each row
+   *  * row_template_extra_vars:                 Extra vars to give to the row template
+   *  *
+   *  * header_template_name:                    Row template path to use to render header
+   *  * header_template_extra_vars:              Extra vars to give to the header template
+   *  *
+   *  * footer_template_name:                    Row template path to use to render footer
+   *  * footer_template_extra_vars:              Extra vars to give to the footer template
    *
    * @param array $options     An array of options
    * @param array $attributes  An array of default HTML attributes
@@ -56,18 +92,25 @@ class sfUoWidgetTable extends sfUoWidget
     parent::configure($options, $attributes);
 
     $this->addOption('data', array());
+    $this->addOption('data_header', array());
     $this->addOption('data_footer', array());
 
     $this->addOption('no_data_message', 'no data');
     $this->addOption('no_data_classname', 'no-data');
 
-    $this->addOption('choice_enable', true);
+    $this->addOption('choice_enable', false);
     $this->addOption('choice_name', 'items');
     $this->addOption('choice_type', 'checkbox');
     $this->addOption('choice_position', 'last');
+    $this->addOption('choice_header', '&nbsp;');
+    $this->addOption('choice_footer', '&nbsp;');
+    $this->addOption('choice_classname', 'choice');
     
-    $this->addOption('row_number_enable', true);
+    $this->addOption('row_number_enable', false);
     $this->addOption('row_number_start', 1);
+    $this->addOption('row_number_header', '#');
+    $this->addOption('row_number_footer', '&nbsp;');
+    $this->addOption('row_number_classname', 'number');
 
     $this->addOption('row_classname_enable', true);
     $this->addOption('row_classname_odd', 'odd');
@@ -91,11 +134,10 @@ class sfUoWidgetTable extends sfUoWidget
   protected function doRender()
   {
     $attributes['class'] = 'table_results';
-    $headers             = $this->getOption('headers');
-    $footer              = $this->getOption('footer');
+    $dataHeader          = $this->getOption('data_header');
     $data                = $this->getOption('data');
 
-    if ((!is_array($data) || !$this->getNbData()) && !(is_array($headers) && count($headers)))
+    if ((!is_array($data) || !$this->getNbData()) && !(is_array($dataHeader) && count($dataHeader)))
     {
       return $this->renderContentTag('p', $this->__($this->getOption('no_data_message')), array('class' => $this->getOption('no_data_classname')));
     }
@@ -122,6 +164,31 @@ class sfUoWidgetTable extends sfUoWidget
    */
   protected function getHeader()
   {
+    $dataHeader = $this->getOption('data_footer');
+    if (!count($dataHeader) && !$this->getOption('header_template_name'))
+    {
+      return '';
+    }
+    
+    $vars         = $this->getOption('header_template_extra_vars');
+    $vars['data'] = $dataHeader;
+    
+    $result = $this->getRow(
+      $this->getOption('header_template_name'), 
+      $vars, 
+      false,
+      false, 
+      $this->getOption('row_number_header'), 
+      $this->getOption('choice_header'),
+      'th'
+    );
+    
+    if (!empty($result))
+    {
+      $result = $this->renderContentTag('thead', $result);
+    }
+    
+    return $result;
   }
   
   /**
@@ -131,6 +198,31 @@ class sfUoWidgetTable extends sfUoWidget
    */
   protected function getFooter()
   {
+    $dataFooter = $this->getOption('data_footer');
+    if (!count($dataFooter) && !$this->getOption('footer_template_name'))
+    {
+      return '';
+    }
+    
+    $vars         = $this->getOption('footer_template_extra_vars');
+    $vars['data'] = $dataFooter;
+    
+    $result = $this->getRow(
+      $this->getOption('footer_template_name'), 
+      $vars, 
+      false,
+      false, 
+      $this->getOption('row_number_footer'), 
+      $this->getOption('choice_footer'),
+      'th'
+    );
+    
+    if (!empty($result))
+    {
+      $result = $this->renderContentTag('tfoot', $result);
+    }
+    
+    return $result;
   }
   
   /**
@@ -140,27 +232,42 @@ class sfUoWidgetTable extends sfUoWidget
    */
   protected function getBody()
   {
-    $result   = '';
-    $data     = $this->getOption('data');
-    $position = $this->getOption('row_number_start');
+    $result = '';
+    $data   = $this->getOption('data');
+    $number = $this->getOption('row_number_start');
 
     if ($this->getNbData())
     {
       $odd = false;
+      $vars                = $this->getOption('row_template_extra_vars');
+      $vars['data']        = $data;
       foreach ($data as $key => $value)
       {
-        $result .= $this->getRow($data, $key, $value, $odd = !$odd, $position);
-        $position ++;
+        $vars['subject_key'] = $key;
+        $vars['subject']     = $value;
+        $vars['odd']         = $odd = !$odd;
+        $vars['number']      = $number;
+      
+        $result .= $this->getRow(
+          $this->getOption('row_template_name'),
+          $vars,
+          $this->getOption('row_classname_enable'),
+          $odd, 
+          $number, 
+          '<input type="'.$this->getOption('choice_type').'" class="'.$this->getOption('choice_type').'" name="'.$this->getOption('choice_name').'['.$this->getId($key, $data).']'.'" />',
+          'td'
+        );
+        $number ++;
       }
     }
     else
     {
-      $headers = $this->getOption('headers');
-      $result .= $this->renderContentTag(
+      $colspan  = $this->getOption('dataHeader') ? count($this->getOption('dataHeader')) : 1;
+      $result  .= $this->renderContentTag(
         'tr', 
         $this->renderContentTag(
           'td', 
-          $this->__($this->getOption('no_data_message')), array('class' => $this->getOption('no_data_classname'), 'colspan' => count($headers))
+          $this->__($this->getOption('no_data_message')), array('class' => $this->getOption('no_data_classname'), 'colspan' => $colspan)
         )
       );
     }
@@ -173,57 +280,59 @@ class sfUoWidgetTable extends sfUoWidget
     return $result;
   }
 
-  protected function getRow($data, $key, $data, $odd, $position)
+  /*
+   * Return a row
+   * 
+   * @param string $template
+   * @param array $templateVars
+   * @param boolean $oddable
+   * @param boolean $odd
+   * @param string $number
+   * @param string $choice
+   * @param string $type Cell type ("td" or "th")
+   *
+   * @return string
+   */
+  protected function getRow($template, array $templateVars, $oddable, $odd, $number, $choice, $type)
   {
-    $vars             = $this->getOption('extra_partial_vars') ? $this->getOption('extra_partial_vars') : array();
-    $vars['data']     = $data;
-    $vars['odd']      = $odd;
-    $vars['position'] = $position;
-    $result           = $this->getTemplate($this->getOption('row_template_name'), $vars);
+    $result = $this->getTemplate($template, $templateVars);
 
-    if ($this->getNbData())
+    if ($this->getOption('row_number_enable'))
     {
-      if ($this->getOption('row_number_enable'))
+      $result = $this->renderContentTag($type, $number, array('class' => $this->getOption('row_number_classname'))) . $result;
+    }
+
+    if ($this->getOption('choice_enable'))
+    {
+      $choiceContent = $this->renderContentTag($type, $choice, array('class' => $this->getOption('choice_classname')));
+      switch ($this->getOption('choice_position'))
       {
-        $result = $this->renderContentTag('td', $position) . $result;
-      }
+        case 'first':
+          $result = $choiceContent.$result;
+          break;
 
-      if ($this->getOption('choice_enable'))
-      {
-        $choice = $this->renderContentTag(
-          'td', 
-          '<input type="'.$this->getOption('choice_type').'" class="'.$this->getOption('choice_type').'" name="'.$this->getOption('choice_name').'['.$this->getId($key, $data).']'.'" />'
-        );
+        case 'last':
+          $result .= $choiceContent;
+          break;
 
-        switch ($this->getOption('choice_position'))
-        {
-          case 'first':
-            $result = $choice.$result;
-            break;
-
-          case 'last':
-            $result .= $choice;
-            break;
-
-          default:
-            throw new Exception ('invalid value for "choice_position" option');
-        }
+        default:
+          throw new Exception ('invalid value for "choice_position" option');
       }
     }
 
     return $this->renderContentTag(
       'tr', 
       $result, 
-      $this->getOption('row_classname_enable') ? array('class' => $odd ? $this->getOption('row_classname_odd') : $this->getOption('row_classname_even')) : array()
+      $oddable ? array('class' => $odd ? $this->getOption('row_classname_odd') : $this->getOption('row_classname_even')) : array()
     );
   }
 
   /**
    * getTemplate - renders HTML output for object row
    *
-   * @param  Doctrine_Record $object
-   * @param  mixed $odd
-   * @param  mixed $position
+   * @param  string $template The template path
+   * @param  array $vars The template vars
+   *
    * @return string
    */
   protected function getTemplate($template, array $vars)
@@ -266,13 +375,16 @@ class sfUoWidgetTable extends sfUoWidget
    */
   public function __clone()
   {
-    if ($this->getOption('data') instanceof sfCallable)
+    foreach (array('data', 'data_header', 'data_footer') as $dataType)
     {
-      $callable = $this->getOption('data')->getCallable();
-      if (is_array($callable))
+      if ($this->getOption($dataType) instanceof sfCallable)
       {
-        $callable[0] = $this;
-        $this->setOption('data', new sfCallable($callable));
+        $callable = $this->getOption($dataType)->getCallable();
+        if (is_array($callable))
+        {
+          $callable[0] = $this;
+          $this->setOption($dataType, new sfCallable($callable));
+        }
       }
     }
   }
