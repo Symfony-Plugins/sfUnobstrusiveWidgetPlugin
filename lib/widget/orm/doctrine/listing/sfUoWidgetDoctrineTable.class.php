7663561f63c0d1d7c27ef1003207b934e534cf51
hugo.hamon <hugo.hamon@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-26 07:05:29 -0600

    [sfUnobstrusiveWidgetPlugin] removed trailing whitespaces and fixed getNbData() method for sfUoWidgetDoctrineTable component
    git-svn-id: http://svn.symfony-project.com/plugins/sfUnobstrusiveWidgetPlugin/trunk@24438 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/widget/orm/doctrine/listing/sfUoWidgetDoctrineTable.class.php
+++ lib/widget/orm/doctrine/listing/sfUoWidgetDoctrineTable.class.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the symfony package.
  * (c) Fabien Potencier <fabien.potencier@symfony-project.com>
- * 
+ *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -18,18 +18,36 @@
  */
 class sfUoWidgetDoctrineTable extends sfUoWidgetTable
 {
+  /**
+   * Configures the widget.
+   *
+   * @param arrya $options
+   * @param array $attributes
+   */
   protected function configure($options = array(), $attributes = array())
   {
     parent::configure($options, $attributes);
 
     $this->addOption('primary_key_getter', 'getId');
   }
-  
+
+  /**
+   * Returns the number of data in the widget.
+   *
+   * @return int
+   */
   public function getNbData()
   {
-    return $this->getOption('data')->count();
+    return count($this->getOption('data'));
   }
-  
+
+  /**
+   * Returns the value referenced at the object's given key.
+   *
+   * @param string $key
+   * @param Doctrine_Record $object
+   * @return mixed
+   */
   protected function getKeyValue($key, $object)
   {
     return call_user_func(array($object, $this->getOption('primary_key_getter')));
